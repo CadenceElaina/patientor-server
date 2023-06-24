bcb73b497a3f047d57223e0881ae559c25444f4d
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2023-06-24 09:45:17 -0400

    ran npm install in hopes that I can host now

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import diagnosisRouter from "./routes/diagnoses";
 import patientRouter from "./routes/patients";
 
-
 const app = express();
 app.use(express.json());
 app.use(cors());
